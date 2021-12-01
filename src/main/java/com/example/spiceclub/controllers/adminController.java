d25621aa45b0452ad983adefeda9312d47370525
SLL <qgh114@alumni.ku.dk>
2021-12-01 13:46:33 +0100

    added admin crud function and a admin directory

--- src/main/java/com/example/spiceclub/controllers/adminController.java
+++ src/main/java/com/example/spiceclub/controllers/adminController.java
@@ -1,7 +1,73 @@
 package com.example.spiceclub.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.spiceclub.models.Spice;
+import com.example.spiceclub.repositories.SpiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class adminController {
+
+
+    @Autowired
+    SpiceRepository spices;
+
+
+    @GetMapping("/spices")
+    public Iterable<Spice> getSpices() {
+
+        return spices.findAll();
+    }
+
+
+    @GetMapping("/spices/{id}")
+    public Spice getSpicesById(@PathVariable Long id) {
+        return spices.findById(id).get();
+    }
+
+
+    @PostMapping("/spices")
+    public Spice addSpices(@RequestBody Spice newspice) {
+
+        return spices.save(newspice);
+    }
+
+
+
+
+    @PatchMapping("/spices/{id}")
+    public void patchSpicesById(@PathVariable Long id, @RequestBody Spice spiceToUpdate) {
+        spices.findById(id).map(foundSpice -> {
+            if (spiceToUpdate.getName() != null) foundSpice.setName(spiceToUpdate.getName());
+            if (spiceToUpdate.getImage() != null) foundSpice.setImage(spiceToUpdate.getImage());
+            if (spiceToUpdate.getDescription() != null) foundSpice.setDescription(spiceToUpdate.getDescription());
+            if (spiceToUpdate.getPrice() != 0) foundSpice.setPrice(spiceToUpdate.getPrice());
+
+            spices.save(foundSpice);
+            return "Spice updated";
+        }).orElse("Spice not found");
+
+
+    }
+
+    @PutMapping("/spice/{id}")
+    public String updateSpiceById(@PathVariable Long id, @RequestBody Spice spiceToUpdate) {
+        if (spices.existsById(id)) {
+            spiceToUpdate.setId(id);
+            spices.save(spiceToUpdate);
+            return "Spice was created";
+        } else {
+            return "Spice not found";
+        }
+    }
+
+
+    @DeleteMapping("/spice/{id}")
+    public void deleteSpicesById(@PathVariable Long id) {
+        spices.deleteById(id);
+    }
+
+
+
+
 }
--- src/main/java/com/example/spiceclub/controllers/spiceController.java
+++ src/main/java/com/example/spiceclub/controllers/spiceController.java
@@ -1,7 +1,30 @@
 package com.example.spiceclub.controllers;
 
+import com.example.spiceclub.models.Spice;
+import com.example.spiceclub.repositories.SpiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class spiceController {
+/*
+    @Autowired
+    SpiceRepository spices;
+
+
+    @GetMapping("/spices")
+    public Iterable<Spice> getSpices() {
+
+        return spices.findAll();
+    }
+
+
+    @GetMapping("/spices/{id}")
+    public Spice getSpicesById(@PathVariable Long id) {
+        return spices.findById(id).get();
+    }
+
+ */
 }
