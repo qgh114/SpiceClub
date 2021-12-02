500f920422d5a6a632f9c035d81ed6b7fcf9fb4e
acmve <acmverden@hotmail.com>
2021-12-02 14:52:36 +0100

    "done med test uden id-test"

--- src/test/java/com/example/spiceclub/AdminRepositoryTest.java
+++ src/test/java/com/example/spiceclub/AdminRepositoryTest.java
@@ -5,6 +5,7 @@ import com.example.spiceclub.models.Spice;
 import com.example.spiceclub.models.SuperAdmin;
 import com.example.spiceclub.repositories.AdminRepository;
 import com.example.spiceclub.repositories.SpiceRepository;
+import com.example.spiceclub.repositories.SuperAdminRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +13,14 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.util.Optional;
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(false)
 public class AdminRepositoryTest {
 
+    // altså superAdmin kan lave CRUD i Admin table
 
     @Autowired
     private AdminRepository admins;
@@ -25,13 +29,48 @@ public class AdminRepositoryTest {
     public void testCreateAdmin() {
 
         Admin admin = new Admin();
-        admin.setEmail("Peter@hotmail.com");
+        admin.setEmail("Lærke@hotmail.com");
         admin.setPassword("1234k");
-
-
 
         Admin savedAdmin = admins.save(admin);
         Assertions.assertNotEquals(savedAdmin, null);
+    }
+
+    @Test
+    public void testUpdateAdmin() {
+        Long adminId = 1L;
+
+        Optional<Admin> optionalAdmin = admins.findById(adminId);
+        Admin admin = optionalAdmin.get();
+
+        admin.setEmail("UpdatedLærke@gmail.com");
+        admins.save(admin);
+
+        Admin updatedAdmin = admins.findById(adminId).get();
+        Assertions.assertEquals(updatedAdmin.getEmail(), "UpdatedLærke@gmail.com");
+
+    }
+
+    @Test
+    public void testReadAdmin() {
+        Long adminId = 1L;
+
+        Optional<Admin> optionalAdmin = admins.findById(adminId);
+        Assertions.assertTrue(optionalAdmin.isPresent());
+
+    }
+
+    @Test
+    public void testDeleteAdmin() {
+
+        Long adminId = 1L;
+        admins.deleteById(adminId);
+
+        Optional<Admin> optionalAdmin = admins.findById(adminId);
+        Assertions.assertFalse(optionalAdmin.isPresent());
+
+
+
 
 
     }
